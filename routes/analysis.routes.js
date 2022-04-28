3fe73746830f323d50b1954900bc92b6e190a28c
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-04-28 16:28:17 +0100

    analysis get router done(Missing test)

--- routes/analysis.routes.js
+++ routes/analysis.routes.js
@@ -8,7 +8,7 @@ const Analysis = require("../models/Analysis.model");
 const router = Router();
 
 //POST - Vai criar uma nova análise e publicá-la no banco de dados
-router.post('/analysis', async (req, res) => {
+router.post('/profile', async (req, res) => {
 
 
 try {
@@ -16,10 +16,34 @@ try {
 } catch (error) {
     
 }
-})
+});
 
 //GET - Vai puxar a análise salva da pessoa
+router.get('/profile', async (req, res) => {
+   try {
+    const userId = req.user.id;
+    const allAnalysis = await Analysis.find({user: userId})
+    res.status(200).json(allAnalysis)
+   } catch (error) {
+    res.status(error.status || 500).json({ place: "Cannot find user analysis in DB, maybe there is none", error: error.message })  
+   } 
+});
+
 
 //PUT - Vai poder alterar a análise
+router.put('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
 
 //DELETE - Vai deletar a análise feita pelo usuário
+router.delete('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
