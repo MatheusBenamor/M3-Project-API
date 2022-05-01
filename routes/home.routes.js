f0ecef1784a4f50da019a06cc090775923aabdfc
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-05-01 13:14:05 +0100

    new deploy test

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('SWOT API')
+    res.send('SWOT API test')
 })
 
 module.exports = router;
