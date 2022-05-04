b967d5a258b7275e01a54ce8a43460bc95aac012
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-05-04 16:56:42 +0100

    just a test for a new analysis model

--- models/Analysis.model.js
+++ models/Analysis.model.js
@@ -6,6 +6,184 @@ const analysisSchema = new Schema(
       type: String,
       required: true,
     },
+    strengths1: {
+      type: String,
+      required: true,
+    },
+    strengthsRel1: {
+      type: Number,
+      required: true,
+    },
+    strengths2: {
+      type: String,
+      required: true,
+    },
+    strengthsRel2: {
+      type: Number,
+      required: true,
+    },
+    strengths3: {
+      type: String,
+      required: true,
+    },
+    strengthsRel3: {
+      type: Number,
+      required: true,
+    },
+    strengths4: {
+      type: String,
+      required: true,
+    },
+    strengthsRel4: {
+      type: Number,
+      required: true,
+    },
+    strengths5: {
+      type: String,
+      required: true,
+    },
+    strengthsRel5: {
+      type: Number,
+      required: true,
+    },
+    weaknesses1: {
+      type: String,
+      required: true,
+    },
+    weaknessesRel1: {
+      type: Number,
+      required: true,
+    },
+    weaknesses2: {
+      type: String,
+      required: true,
+    },
+    weaknessesRel2: {
+      type: Number,
+      required: true,
+    },
+    weaknesses3: {
+      type: String,
+      required: true,
+    },
+    weaknessesRel3: {
+      type: Number,
+      required: true,
+    },
+    weaknesses4: {
+      type: String,
+      required: true,
+    },
+    weaknessesRel4: {
+      type: Number,
+      required: true,
+    },
+    weaknesses5: {
+      type: String,
+      required: true,
+    },
+    weaknessesRel5: {
+      type: Number,
+      required: true,
+    },
+    opportunities1: {
+      type: String,
+      required: true,
+    },
+    opportunitiesRel1: {
+      type: Number,
+      required: true,
+    },
+    opportunities2: {
+      type: String,
+      required: true,
+    },
+    opportunitiesRel2: {
+      type: Number,
+      required: true,
+    },
+    opportunities3: {
+      type: String,
+      required: true,
+    },
+    opportunitiesRel3: {
+      type: Number,
+      required: true,
+    },
+    opportunities4: {
+      type: String,
+      required: true,
+    },
+    opportunitiesRel4: {
+      type: Number,
+      required: true,
+    },
+    opportunities5: {
+      type: String,
+      required: true,
+    },
+    opportunitiesRel5: {
+      type: Number,
+      required: true,
+    },
+    threats1: {
+      type: String,
+      required: true,
+    },
+    threatsRel1: {
+      type: Number,
+      required: true,
+    },
+    threats2: {
+      type: String,
+      required: true,
+    },
+    threatsRel2: {
+      type: Number,
+      required: true,
+    },
+    threats3: {
+      type: String,
+      required: true,
+    },
+    threatsRel3: {
+      type: Number,
+      required: true,
+    },
+    threats4: {
+      type: String,
+      required: true,
+    },
+    threatsRel4: {
+      type: Number,
+      required: true,
+    },
+    threats5: {
+      type: String,
+      required: true,
+    },
+    threatsRel5: {
+      type: Number,
+      required: true,
+    },  
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+  }
+},
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = model("Analysis", analysisSchema);
+
+/*const analysisSchema = new Schema(
+  {
+    analysisName: {
+      type: String,
+      required: true,
+    },
     
     strengths: 
        [{
@@ -53,6 +231,4 @@ const analysisSchema = new Schema(
   {
     timestamp: true,
   }
-);
-
-module.exports = model("Analysis", analysisSchema);
+);*/
